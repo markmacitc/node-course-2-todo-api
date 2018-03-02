7e817f4e10ae974f3aca783331abad1a37efa9f8
Mark McDonald <longryder@gmail.com>
2018-03-02 16:50:18 +0000

    Reverted to working copy of mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,22 @@
 var mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+                //copy the link from "To connect using a driver via the standard MongoDB URI" section
+                //insert db user name and password here
+                const REMOTE_MONGO = 'mongodb://todouser:mongodb1.@ds153778.mlab.com:53778/todoapp';
+                const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
+                const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
+
+                mongoose.Promise = global.Promise;
+                mongoose.connect(MONGO_URI).then(() => {
+                    console.log('Connected to Mongo instance.')
+                }, (err) => {
+                    console.log('Error connecting to Mongo instance: ', err);
+                });
+
+                module.export = { mongoose }
+
+
+
+
 
 module.exports = {mongoose};
